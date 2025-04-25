d136c9793f67c856dde4278803ce12713f6835c9
Bipin Swarnkar <vipinswarnkar1989@gmail.com>
2025-04-25 12:42:17 +0200

    Add traverse the list

--- top-hundred-ques/arr/singlyLinkedList.js
+++ top-hundred-ques/arr/singlyLinkedList.js
@@ -1,1 +1,22 @@
 
+function Node(data) {
+  this.val = data;
+  this.next = null;
+}
+
+function traverseLinkedList(head){
+  while(head !== null){
+    console.log(head.val)
+    head = head.next;
+  }
+}
+
+// Create a hard-coded linked list:
+    // 10 -> 20 -> 30 -> 40
+    let head = new Node(10);
+    head.next = new Node(20);
+    head.next.next = new Node(30);
+    head.next.next.next = new Node(40);
+
+    // Example of traversing the node and printing
+    traverseLinkedList(head);
