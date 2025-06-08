09927c59801bae871aca24a1a9043ce9a2fb9f91
Bipin Swarnkar <vipinswarnkar1989@gmail.com>
2025-06-08 16:57:11 +0530

    Update palindromeLinkedList.js
    Add optimized solution

--- top-hundred-ques/arr/palindromeLinkedList.js
+++ top-hundred-ques/arr/palindromeLinkedList.js
@@ -13,3 +13,42 @@ var isPalindrome = function(head) {
   }
     return true;
 };
+
+var isPalindromeOptimized = function(head) {
+    if (!head || !head.next) return true;
+    
+    let slow = head, fast = head;
+    
+    // Step 1: Find the middle
+    while(fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    
+    //when fast will be at last or null slow will be at middle
+    // // Step 2: Reverse second half
+    let reversedMiddleHalf = reverseLl(slow);
+    let curr = head;
+    
+    // check if palindrome
+    while(reversedMiddleHalf) {
+        if(reversedMiddleHalf.val !== curr.val) {
+            return false
+        }
+        reversedMiddleHalf = reversedMiddleHalf.next;
+        curr = curr.next;
+    }
+    return true;
+};
+
+function reverseLl(head) {
+    let curr = head, prev = null;
+    
+    while(curr) {
+        let next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
