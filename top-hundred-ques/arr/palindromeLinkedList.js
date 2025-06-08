7436b53496ab9fb9a7fe1c4485a5e2272bd26a39
Bipin Swarnkar <vipinswarnkar1989@gmail.com>
2025-06-08 14:13:57 +0530

    Update palindromeLinkedList.js
    add naive solution

--- top-hundred-ques/arr/palindromeLinkedList.js
+++ top-hundred-ques/arr/palindromeLinkedList.js
@@ -1,1 +1,15 @@
-
+var isPalindrome = function(head) {
+  let arr = [], curr = head
+  
+  while(curr) {
+      arr.push(curr.val);
+      curr = curr.next
+  }
+    
+  for(let i = 0;i < arr.length/2;i++) {
+      if(arr[i] !== arr[arr.length - i - 1]){
+          return false;
+      }
+  }
+    return true;
+};
